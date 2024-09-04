0297bec5a93ff88579b94e845fb7cf5488db2f67
Daniel Smith <memorysight@gmail.com>
2024-09-04 19:42:18 -0400

    working intro avatar

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,13 +1,15 @@
-function App() {
+import { useState } from "react";
+import Controller from "./components/Controller";
 
+function App() {
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="">
-      <div className="text-2x bg-blue-500">Hekki</div>
+    <div>
+      <>
+        <Controller />
+      </>
     </div>
-
-    
-  )
+  );
 }
-
 export default App;
