35c6257a7462f6b7bb56bd0abf37c2236a8c1f74
Daniel Smith <memorysight@gmail.com>
2024-09-04 15:57:30 -0400

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+
+# from fastapi import FastAPI, File, UploadFile, HTTPException
+# from fastapi.responses import StreamingResponse
+# from fastapi.middleware.cors import CORSMiddleware
+# from decouple import config
+# import openai
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# async def root():
+#     print("daniel")
+#     return {"message": "asfff"}
+
+
+
+
+
---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+
+  return (
+    <div className="">
+      <div className="text-2x bg-blue-500">Hekki</div>
+    </div>
+
+    
+  )
+}
+
+export default App;
