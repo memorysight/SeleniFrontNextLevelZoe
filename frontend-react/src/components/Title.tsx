ffa0c726e8ee1964361cf953e125962fcb24eb53
Daniel Smith <memorysight@gmail.com>
2024-09-09 11:09:05 -0400

    updated and working but Spanish

--- frontend-react/src/components/Title.tsx
+++ frontend-react/src/components/Title.tsx
@@ -30,7 +30,7 @@ function Title({ setMessages }: Props) {
 
   return (
     <div className="flex justify-between items-center w-full p-4 bg-gray-900 text-white font-bold shadow">
-      <div className="italic">Zoe</div>
+      <div className="italic">Zoe:   Advanced AI System</div>
       <button
         onClick={resetConversation}
         className={
